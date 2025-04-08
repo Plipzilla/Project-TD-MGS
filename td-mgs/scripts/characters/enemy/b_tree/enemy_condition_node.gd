7ad1f246850e44727b729ffb50476a479f98961b
Christopher Seven Phiri <chris7phiri@gmail.com>
2025-04-08 23:55:11 +0200

    init enemy
    clean btree stuff
    
    Revert "clean btree stuff"
    
    This reverts commit 7358b48715bd09d4896efde78d890dc970bdef37.

--- td-mgs/scripts/characters/enemy/b_tree/enemy_condition_node.gd
+++ td-mgs/scripts/characters/enemy/b_tree/enemy_condition_node.gd
@@ -1,25 +1,25 @@
-class_name ConditionNode extends BehaviorNode
+class_name EnemyConditionNode extends BehaviorNode
 
-var condition_name: String
+var condition_name: EnemyEnums.ConditionType
 
-func _init(name: String):
+func _init(name: EnemyEnums.ConditionType):
 	condition_name = name
 
 func tick() -> int:
 	# Override in specific implementations
 	match condition_name:
-		"IsPlayerVisible":
+		EnemyEnums.ConditionType.IS_PLAYER_VISIBLE:
 			return _is_player_visible()
-		"IsPlayerInRange":
+		EnemyEnums.ConditionType.IS_PLAYER_IN_RANGE:
 			return _is_player_in_range()
 	return BEnums.NodeStatus.FAILURE
 
 func _is_player_visible() -> int:
 	# Would check game state
 	print("Checking if player is visible")
-	return BEnums.NodeStatus.SUCCESS # For example purposes
+	return BEnums.NodeStatus.FAILURE
 
 func _is_player_in_range() -> int:
 	# Would check game state
 	print("Checking if player is in attack range")
-	return BEnums.NodeStatus.SUCCESS # For example purposes
+	return BEnums.NodeStatus.FAILURE
---
+++ td-mgs/scripts/characters/enemy/states/idle_state.gd
@@ -0,0 +1,23 @@
+class_name IdleState extends EnemyState
+
+@export var anim_name : String = "idle"
+
+@export_category("AI")
+@export var state_duration_min : float = 0.5
+@export var state_duration_max : float = 1.5
+@export var after_idle_state : Node
+
+var _timer : float = 0.
+
+## What happens when the enemy enters this State?
+func enter() -> void:
+	enemy.velocity = Vector2.ZERO
+	_timer = randf_range( state_duration_min, state_duration_max )
+	enemy.update_animation( anim_name )
+	pass
+	
+func process( _delta : float ) -> EnemyState:
+	_timer -= _delta
+	if _timer <= 0:
+		return after_idle_state as EnemyState
+	return null
